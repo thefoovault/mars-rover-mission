cd255ac98002cb7b5ef85d1b71098480b06b9c83
Thefoovault <marta.ag.ma@gmail.com>
2021-05-28 20:51:00 +0200

    Added minimal buses and controller structure, using Symfony messenger.

---
+++ src/Shared/Infrastructure/Symfony/Controller/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Infrastructure\Symfony\Controller;
+
+use Shared\Domain\Bus\Command\Command;
+use Shared\Domain\Bus\Command\CommandBus;
+use Shared\Domain\Bus\Query\Query;
+use Shared\Domain\Bus\Query\QueryBus;
+use Shared\Domain\Bus\Query\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class Controller extends AbstractController
+{
+    public function __construct(
+        protected QueryBus $queryBus,
+        protected CommandBus $commandBus
+    ) {}
+
+    protected function ask(Query $query): ?Response
+    {
+        return $this->queryBus->ask($query);
+    }
+
+    protected function dispatch(Command $command): void
+    {
+        $this->commandBus->dispatch($command);
+    }
+}
