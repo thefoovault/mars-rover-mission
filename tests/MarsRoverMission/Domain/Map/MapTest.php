910351f0668501abaf1b7f45f2bd224ad1c41ab9
Thefoovault <marta.ag.ma@gmail.com>
2021-05-31 20:12:08 +0200

    Minor refactor to use MapFaker in Map test

--- tests/MarsRoverMission/Domain/Map/MapTest.php
+++ tests/MarsRoverMission/Domain/Map/MapTest.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Test\MarsRoverMission\Domain\Map;
 
-use MarsRoverMission\Domain\Map\Dimensions;
-use MarsRoverMission\Domain\Map\Height;
-use MarsRoverMission\Domain\Map\Map;
 use MarsRoverMission\Domain\Map\Obstacles;
-use MarsRoverMission\Domain\Map\Width;
 use PHPUnit\Framework\TestCase;
+use Test\MarsRoverMission\Application\FakeMap;
 use Traversable;
 
 final class MapTest extends TestCase
@@ -17,13 +14,7 @@ final class MapTest extends TestCase
     /** @test */
     public function shouldCreateValidMap(): void
     {
-        $map = new Map(
-            new Dimensions(
-                new Width(10),
-                new Height(20)
-            ),
-            new Obstacles([])
-        );
+        $map = FakeMap::create();
 
         $this->assertEquals(10, $map->dimensions()->width()->value());
         $this->assertEquals(20, $map->dimensions()->height()->value());
