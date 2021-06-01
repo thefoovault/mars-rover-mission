79eab39be5bd7dd497f00d6e78d8d0bf4b1c294f
Thefoovault <marta.ag.ma@gmail.com>
2021-06-01 15:30:50 +0200

    Added constants in the Map faker.

--- tests/MarsRoverMission/Domain/Map/MapTest.php
+++ tests/MarsRoverMission/Domain/Map/MapTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Test\MarsRoverMission\Domain\Map;
 
+use MarsRoverMission\Domain\Map\Obstacle;
 use MarsRoverMission\Domain\Map\Obstacles;
 use PHPUnit\Framework\TestCase;
 use Test\MarsRoverMission\FakeMap;
@@ -15,10 +16,15 @@ final class MapTest extends TestCase
     public function shouldCreateValidMap(): void
     {
         $map = FakeMap::create();
+        $iterator = $map->obstacles()->getIterator();
+        $obstacle = $iterator->current();
 
-        $this->assertEquals(10, $map->dimensions()->width()->value());
-        $this->assertEquals(20, $map->dimensions()->height()->value());
+        $this->assertEquals(FakeMap::WIDTH, $map->dimensions()->width()->value());
+        $this->assertEquals(FakeMap::HEIGHT, $map->dimensions()->height()->value());
         $this->assertInstanceOf(Obstacles::class, $map->obstacles());
-        $this->assertInstanceOf(Traversable::class, $map->obstacles()->getIterator());
+        $this->assertInstanceOf(Traversable::class, $iterator);
+        $this->assertInstanceOf(Obstacle::class, $obstacle);
+        $this->assertEquals(FakeMap::X_COORDINATE, $obstacle->x()->value());
+        $this->assertEquals(FakeMap::Y_COORDINATE, $obstacle->y()->value());
     }
 }
--- tests/MarsRoverMission/FakeMap.php
+++ tests/MarsRoverMission/FakeMap.php
@@ -16,17 +16,22 @@ use MarsRoverMission\Domain\TwoDimensionalPlane\Width;
 
 final class FakeMap
 {
+    public const WIDTH = 10;
+    public const HEIGHT = 20;
+    public const X_COORDINATE = 2;
+    public const Y_COORDINATE = 2;
+
     public static function create(): Map
     {
         return new Map(
             new Dimensions(
-                new Width(10),
-                new Height(20)
+                new Width(self::WIDTH),
+                new Height(self::HEIGHT)
             ),
             new Obstacles([
                 new Obstacle(
-                    new Coordinates(2),
-                    new Coordinates(2)
+                    new Coordinates(self::X_COORDINATE),
+                    new Coordinates(self::Y_COORDINATE)
                 )
             ])
         );
