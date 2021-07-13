e7ddadea4d365264a5ecb049ae09323780659a43
Thefoovault <marta.ag.ma@gmail.com>
2021-07-13 19:14:38 +0200

    Added logger in RoverRepository

--- src/MarsRoverMission/Infrastructure/Persistence/Rover/JsonRoverRepository.php
+++ src/MarsRoverMission/Infrastructure/Persistence/Rover/JsonRoverRepository.php
@@ -10,13 +10,19 @@ use MarsRoverMission\Domain\Rover\Exception\RoverNotFound;
 use MarsRoverMission\Domain\Rover\FacingDirection;
 use MarsRoverMission\Domain\Rover\Rover;
 use MarsRoverMission\Domain\Rover\RoverRepository;
+use Psr\Log\LoggerInterface;
 
 final class JsonRoverRepository implements RoverRepository
 {
     private const FILE = '../../../etc/data/rover.json';
 
+    public function __construct(
+        private LoggerInterface $logger
+    ){}
+
     public function save(Rover $rover): void
     {
+        $this->logger->info("User save the rover ");
         file_put_contents(self::FILE, json_encode($this->extractRoverToArray($rover)));
     }
 
@@ -33,17 +39,23 @@ final class JsonRoverRepository implements RoverRepository
 
     public function find(): Rover
     {
-        if (!file_exists(self::FILE)) {
-            throw new RoverNotFound();
-        }
-        $roverInfo = json_decode(file_get_contents(self::FILE), true);
+        try {
+            $this->logger->info("User wants to find get the rover data");
+            if (!file_exists(self::FILE)) {
+                throw new RoverNotFound();
+            }
+            $roverInfo = json_decode(file_get_contents(self::FILE), true);
 
-        return new Rover(
-            new PointRover(
-                new Coordinates($roverInfo['coordinates']['x']),
-                new Coordinates($roverInfo['coordinates']['y'])
-            ),
-            new FacingDirection($roverInfo['facingDirection'])
-        );
+            return new Rover(
+                new PointRover(
+                    new Coordinates($roverInfo['coordinates']['x']),
+                    new Coordinates($roverInfo['coordinates']['y'])
+                ),
+                new FacingDirection($roverInfo['facingDirection'])
+            );
+        } catch (RoverNotFound $exception){
+            $this->logger->error('Rover not found!', ['exception' => $exception]);
+            throw $exception;
+        }
     }
 }
